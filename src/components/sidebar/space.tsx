8587421b5594025714e3c389f3af5490a07352c1
longj724 <justinlong596@gmail.com>
2024-08-06 22:40:33 -0400

    Fix bug with spaceId

--- src/components/sidebar/space.tsx
+++ src/components/sidebar/space.tsx
@@ -12,13 +12,13 @@ type Props = {
 };
 
 const Space = ({ space }: Props) => {
-  const { spaceID } = useParams();
+  const { spaceId } = useParams();
 
   return (
     <Link
       href={`/space/${space.id}`}
       className={cn(
-        spaceID === space.id ? 'bg-gray-200' : '',
+        spaceId === space.id ? 'bg-gray-200' : '',
         'flex w-[90%] items-center gap-3 rounded-lg px-5 py-2 text-primary transition-all hover:cursor-pointer hover:bg-gray-200 hover:text-primary'
       )}
     >
