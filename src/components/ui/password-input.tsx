1d38c72bd521dcf516fd95c9212f4c53ff7b0edf
longj724 <justinlong596@gmail.com>
2024-08-30 00:08:41 -0400

    Fix styling issue with inputs

--- src/components/ui/password-input.tsx
+++ src/components/ui/password-input.tsx
@@ -4,18 +4,24 @@ import { Input } from '@/components/ui/input';
 import { EyeIcon, EyeOffIcon } from 'lucide-react';
 
 export interface PasswordInputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  suffix?: React.ReactNode;
+}
 
 const PasswordInput = React.forwardRef<HTMLInputElement, PasswordInputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, suffix, ...props }, ref) => {
     const [showPassword, setShowPassword] = React.useState(false);
 
     return (
-      <Input
-        className={className}
-        {...props}
-        suffix={
-          showPassword ? (
+      <div className="flex gap-2 items-center w-full">
+        <Input
+          className={className}
+          {...props}
+          type={showPassword ? 'text' : 'password'}
+          ref={ref}
+        />
+        <div className="hover:cursor-pointer">
+          {showPassword ? (
             <EyeIcon
               className="select-none"
               onClick={() => setShowPassword(false)}
@@ -25,11 +31,9 @@ const PasswordInput = React.forwardRef<HTMLInputElement, PasswordInputProps>(
               className="select-none"
               onClick={() => setShowPassword(true)}
             />
-          )
-        }
-        type={showPassword ? 'text' : 'password'}
-        ref={ref}
-      />
+          )}
+        </div>
+      </div>
     );
   }
 );
