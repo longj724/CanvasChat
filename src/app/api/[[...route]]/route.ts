b86022b7a013caa18baedd4fa9d90314d08bfca4
longj724 <justinlong596@gmail.com>
2024-09-18 22:31:45 -0400

    Debug cors middleware

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -12,7 +12,33 @@ import users from './users';
 export const runtime = 'nodejs';
 
 const app = new Hono().basePath('/api');
-app.use('/api/*', cors());
+// Debug middleware
+app.use('*', async (c, next) => {
+  console.log('Incoming request:', c.req.method, c.req.url);
+  console.log('Request headers:', c.req.header());
+  await next();
+  console.log('Response status:', c.res.status);
+  console.log('Response headers:', c.res.headers);
+});
+
+app.use(
+  '*',
+  cors({
+    origin: ['https://canvaschat.xyz', 'https://www.canvaschat.xyz'],
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowHeaders: [
+      'Accept',
+      'Authorization',
+      'Content-Type',
+      'Origin',
+      'X-Requested-With',
+    ],
+    exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+    maxAge: 600,
+    credentials: true,
+  })
+);
+
 app.use('*', clerkMiddleware());
 
 const routes = app
